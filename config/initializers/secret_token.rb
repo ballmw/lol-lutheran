0564ed75df978325246f122bbf800ef200430fd3
Michael Ball <mwball@gmail.com>
2011-03-16 19:41:47 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LolLutheran::Application.config.secret_token = '2581c2114580aec1f335e8de4719ca3b708a1e343b51ec53cd1ee6a68038c76ca211c7255f7507c825831d869d8004a6d0630087116db3e81d4d84c910770ecd'
